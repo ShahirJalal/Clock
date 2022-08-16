83b539b917239f69cbcc98607880b71988d0f547
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-08-16 08:18:31 +0800

    Update

---
+++ MyFrame.java
@@ -0,0 +1,17 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("My Clock Program");
+        this.setLayout(new FlowLayout());
+        this.setSize(350, 200);
+        this.setResizable(false);
+        this.setVisible(true);
+
+    }
+    
+}
