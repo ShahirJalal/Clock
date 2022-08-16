0cb14805b3f7768ab8e0d3186050893b91cf48b3
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-08-16 08:19:12 +0800

    Update MyFrame.java

--- MyFrame.java
+++ MyFrame.java
@@ -1,8 +1,16 @@
 import java.awt.FlowLayout;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class MyFrame extends JFrame {
+
+    Calendar calendar;
+    SimpleDateFormat timeFormat;
+    JLabel timLabel;
+    String time;
 
     MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -10,7 +18,18 @@ public class MyFrame extends JFrame {
         this.setLayout(new FlowLayout());
         this.setSize(350, 200);
         this.setResizable(false);
+
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+
+        timLabel = new JLabel();
+
+        time = timeFormat.format(Calendar.getInstance().getTime());
+        timLabel.setText(time);
+
+        this.add(timLabel);
         this.setVisible(true);
+
+        setTime();
 
     }
     
